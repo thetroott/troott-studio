6d4cd023527dc10849614c48803295ca7dd62030
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:32:21 +0100

    feat: add interface
    Add interface to this codebase.

---
+++ src/api/interface.ts
@@ -0,0 +1,4 @@
+export type { IStorage } from '@/api/storage.types';
+export type { IAppContext, IClearResource } from '@/context/app/types';
+
+export type { ICollection } from '@/utils/interfaces.util';
