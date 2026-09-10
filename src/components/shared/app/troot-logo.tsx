7c86fd9096ac4e0378ce1461dcdb1ef74713aa3a
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:32:27 +0100

    feat: add troot logo page
    Add the troot logo page to this app.

---
+++ src/components/shared/app/troot-logo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+type TroottLogoProps = React.ImgHTMLAttributes<HTMLImageElement>;
+
+export const TroottLogo: React.FC<Omit<TroottLogoProps, 'src' | 'alt'>> = (
+    props,
+) => {
+
+
+    // const logoSrc =
+    //     resolvedTheme === 'dark'
+    //         ? '/images/assets/troott-icon.svg'
+    //         : '/images/assets/troott-icon-dark.svg';
+    const logoSrc = '/images/assets/troott-icon-dark.svg';
+
+    return (
+        <img
+            src={logoSrc}
+            alt="Troott Logo"
+            width={100}
+            height={100}
+            {...props}
+        />
+    );
+};
