340bd202548ab9b06728fa09aa09d46fa3dfcd21
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:32:32 +0100

    feat: add breadcrumb map component
    Add the breadcrumb map component to this app.

---
+++ src/components/shared/navigation/breadcrumb-map.tsx
@@ -0,0 +1,48 @@
+const BreadcrumbMap: Record<string, string> = {
+    '/dashboard': 'Dashboard',
+    '/dashboard/sermons': 'Sermons',
+    '/dashboard/sermons/create': 'Create Sermon',
+    '/dashboard/settings': 'Settings',
+    '/sermons': 'Your Sermons',
+    '/sermons/create': 'Create Sermon',
+    '/settings': 'Settings',
+    '/upload': 'Upload Sermon',
+    '/analytics': 'Sermon Analytics',
+    '/bin': 'Bin',
+    '/get-started': 'Get started',
+    '/get-started/verify-account': 'Verify your account',
+    '/get-started/verify-account/personal-information': 'Personal information',
+    '/get-started/verify-account/verify-document': 'Document verification',
+    '/get-started/verify-account/verify-document/document1':
+        'Document verification',
+    '/get-started/verify-account/verify-document/select':
+        'Select Document Type ',
+    '/get-started/verify-account/verify-document/start': 'Start Verification',
+    '/get-started/home-address/home-address': 'Home address',
+    '/get-started/ministry-input/ministry-input': 'Ministry input',
+};
+
+export default BreadcrumbMap;
+
+// const BreadcrumbMap: Record<string, string> = {
+//     "/dashboard": "Dashboard",
+//     "/dashboard/sermons": "Sermons",
+//     "/dashboard/sermons/create": "Create Sermon",
+//     "/dashboard/settings": "Settings",
+//     "/upload-sermon": "Sermons",
+//     "/get-sermons": "Your Sermons",
+//     "/my-series": "Your Series",
+//     "/user-draft": "Drafts",
+//     "/user-trash": "Trash",
+//     "/my-analytics": "Sermon Analytics",
+//     "/get-started": "Get started",
+//     "/get-started/verify-account": "Verify your account",
+//     "/get-started/verify-account/personal-information": "Personal information",
+//     "/get-started/verify-account/verify-document": "Document verification",
+//     "/get-started/verify-account/verify-document/select": " ",
+//     "/get-started/verify-account/verify-document/upload": "Select Document Type",
+//     "/get-started/verify-account/verify-document/start": "Start Verification",
+//     // add more as needed
+//   };
+
+// export default BreadcrumbMap;
