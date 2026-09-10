775242235ffb7bdc3ab88f46e176201e1d9cbedf
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:32:19 +0100

    feat: add mysermonFeatures component
    Add the mysermonFeatures component to this app.

---
+++ src/_data/mysermonFeatures.tsx
@@ -0,0 +1,17 @@
+export const features = [
+    {
+        title: 'Build Your Sermon Library',
+        description:
+            'Organize your teachings into a personal collection that your audience can easily revisit anytime.',
+    },
+    {
+        title: 'Create Impactful Series',
+        description:
+            'Group related sermons into powerful series that guide your listeners through deep, spiritual journeys.',
+    },
+    {
+        title: 'Inspire and Reach More People',
+        description:
+            'Every sermon you upload is a chance to bless someone. Track your reach, receive feedback, and help others grow.',
+    },
+];
