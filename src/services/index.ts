645c8d6ec6266abc96c87a4b90aa237868e2cc01
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-09-10 23:32:45 +0100

    feat: add index
    Add index to this codebase.

---
+++ src/services/index.ts
@@ -0,0 +1,14 @@
+export { ObservabilityUserSync } from './observability/ObservabilityUserSync';
+export {
+    ErrorLoggingService,
+    errorLogger,
+} from './error/ErrorLoggingService';
+export {
+    startSermonAudioUpload,
+    type StartSermonAudioUploadParams,
+    type StartSermonAudioUploadResult,
+} from './upload/sermon-upload.service';
+export {
+    uploadStorageFile,
+    type StorageUploadResult,
+} from './upload/storage-upload.service';
